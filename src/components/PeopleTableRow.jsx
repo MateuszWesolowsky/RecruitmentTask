eef277910f3094639c1fe0a949e02d2d0e5c98dc
Mateusz <m.wesolowski.92@gmail.com>
2025-02-02 14:49:46 +0100

    create peopleRows static component

---
+++ src/components/PeopleTableRow.jsx
@@ -0,0 +1,13 @@
+export const PeopleTableRow = () => {
+  return (
+    <tr className="text-center hover:bg-gray-100">
+      <td className="border border-gray-400 p-2">Mateusz</td>
+      <td className="border border-gray-400 p-2">Mateusz</td>
+      <td className="border border-gray-400 p-2">Mateusz</td>
+      <td className="border border-gray-400 p-2">Mateusz</td>
+      <td className="border border-gray-400 p-2">190 cm</td>
+      <td className="border border-gray-400 p-2">100 kg</td>
+      <td className="border border-gray-400 p-2">1992</td>
+    </tr>
+  );
+};
