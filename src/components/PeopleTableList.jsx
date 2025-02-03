1d88a52b97efa5d3632e7c38c7b3872991374b8f
Mateusz <m.wesolowski.92@gmail.com>
2025-02-03 20:14:57 +0100

    add map fn to display rowns in table

--- src/components/PeopleTableList.jsx
+++ src/components/PeopleTableList.jsx
@@ -1,22 +1,28 @@
+import { useContext } from 'react';
+import { DataContext } from '../context/DataContext';
 import { PeopleTableRow } from './PeopleTableRow';
 
 export const PeopleTableList = () => {
+  const { state } = useContext(DataContext);
+
   return (
     <div className="overflow-x-auto">
       <table className="table-auto w-full bg-white border border-gray-300 shadow-lg">
         <thead>
           <tr className="bg-gray-100 text-center">
             <th className="border border-gray-400 p-2">Name</th>
-            <th className="border border-gray-400 p-2">Name</th>
-            <th className="border border-gray-400 p-2">Name</th>
-            <th className="border border-gray-400 p-2">Name</th>
             <th className="border border-gray-400 p-2">Height</th>
             <th className="border border-gray-400 p-2">Mass</th>
             <th className="border border-gray-400 p-2">Birth Year</th>
+            <th className="border border-gray-400 p-2">Eye color</th>
+            <th className="border border-gray-400 p-2">Hair color</th>
+            <th className="border border-gray-400 p-2">Skin color</th>
           </tr>
         </thead>
         <tbody>
-          <PeopleTableRow />
+          {state.data?.map((person) => (
+            <PeopleTableRow data={person} key={Math.random()} />
+          ))}
         </tbody>
       </table>
     </div>
