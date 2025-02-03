b62e6e9467a752dcdffb6be55b552198ada7967a
Mateusz <m.wesolowski.92@gmail.com>
2025-02-03 15:39:02 +0100

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => (
+  <header className="bg-blue-600 text-white py-6 px-4 rounded-md shadow-md mb-4 text-center">
+    <h1 className="text-3xl font-bold">Star Wars Data Explorer</h1>
+    <p className="mt-2 text-lg">
+      Poznaj wszechświat Gwiezdnych Wojen! Przeglądaj dane dotyczące postaci i
+      planet. Skorzystaj z zakładek, aby przełączać się między kategoriami i
+      odkrywać fascynujące informacje.
+    </p>
+  </header>
+);
