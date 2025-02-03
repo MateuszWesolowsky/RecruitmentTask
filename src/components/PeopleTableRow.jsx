bcc4721b3f4fb2336cdeac11b5694fbe381ad5eb
Mateusz <m.wesolowski.92@gmail.com>
2025-02-03 20:13:07 +0100

    display data in table rows

--- src/components/PeopleTableRow.jsx
+++ src/components/PeopleTableRow.jsx
@@ -1,13 +1,15 @@
-export const PeopleTableRow = () => {
+export const PeopleTableRow = ({ data }) => {
   return (
-    <tr className="text-center hover:bg-gray-100">
-      <td className="border border-gray-400 p-2">Mateusz</td>
-      <td className="border border-gray-400 p-2">Mateusz</td>
-      <td className="border border-gray-400 p-2">Mateusz</td>
-      <td className="border border-gray-400 p-2">Mateusz</td>
-      <td className="border border-gray-400 p-2">190 cm</td>
-      <td className="border border-gray-400 p-2">100 kg</td>
-      <td className="border border-gray-400 p-2">1992</td>
-    </tr>
+    <>
+      <tr className="text-center hover:bg-gray-100">
+        <td className="border border-gray-400 p-2">{data.name}</td>
+        <td className="border border-gray-400 p-2">{data.height} cm</td>
+        <td className="border border-gray-400 p-2">{data.mass} kg</td>
+        <td className="border border-gray-400 p-2">{data.birth_year}</td>
+        <td className="border border-gray-400 p-2">{data.eye_color}</td>
+        <td className="border border-gray-400 p-2">{data.hair_color}</td>
+        <td className="border border-gray-400 p-2">{data.skin_color}</td>
+      </tr>
+    </>
   );
 };
