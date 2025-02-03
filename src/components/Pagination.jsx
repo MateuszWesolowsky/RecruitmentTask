4086f2f2ee8d3828fe5ab2ebdefc1568f75152a1
Mateusz <m.wesolowski.92@gmail.com>
2025-02-03 15:37:04 +0100

    add fn dynamic pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,28 @@
+import { useContext } from 'react';
 import { Button } from './Button';
+import { DataContext } from '../context/DataContext';
 
 export const Pagination = () => {
+  const { state, dispatch } = useContext(DataContext);
+
+  const handlePreviousePageClick = () => {
+    if (state.currentPage > 1) {
+      dispatch({ type: 'SET_PAGE', payload: state.currentPage - 1 });
+    }
+  };
+  const handleNextPageClick = () => {
+    if (state.currentPage < state.totalPages) {
+      dispatch({ type: 'SET_PAGE', payload: state.currentPage + 1 });
+    }
+  };
+
   return (
     <div className="flex gap-4 justify-center items-center mt-4">
-      <Button onClick={() => console.log('-')}>Previous</Button>
-      <p className="text-center">Page 1 of 2</p>
-      <Button onClick={() => console.log('+')}>Next</Button>
+      <Button onClick={() => handlePreviousePageClick()}>Previous</Button>
+      <p className="text-center">
+        Page {state.currentPage} of {state.totalPages}
+      </p>
+      <Button onClick={() => handleNextPageClick()}>Next</Button>
     </div>
   );
 };
