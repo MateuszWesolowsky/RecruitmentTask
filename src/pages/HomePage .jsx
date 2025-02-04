210de4454e0d2c5f6fc188154de6b7dae1829b0d
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 16:50:42 +0100

    add props to header component

--- src/pages/HomePage .jsx
+++ src/pages/HomePage .jsx
@@ -6,7 +6,12 @@ import { PeopleTableList } from '../components/PeopleTableList';
 export const HomePage = () => {
   return (
     <div className="p-1 sm:p-4 mt-8 ">
-      <Header />
+      <Header
+        title={'Star Wars Data Explorer'}
+        description={
+          'Poznaj wszechświat Gwiezdnych Wojen! Przeglądaj dane dotyczące postaci. Skorzystaj z zakładek, aby przełączać się między kategoriami i odkrywać fascynujące informacje.'
+        }
+      />
       <Navigation />
       <PeopleTableList />
       <Pagination />
