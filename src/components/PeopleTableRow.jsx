354c6a5e7a231a5cba255975b104aeb9c730b99a
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 22:29:46 +0100

    split url to get id

--- src/components/PeopleTableRow.jsx
+++ src/components/PeopleTableRow.jsx
@@ -1,18 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 
-export const PeopleTableRow = ({ data, index }) => {
+export const PeopleTableRow = ({ data }) => {
   const navigate = useNavigate();
   const { name, height, mass, birth_year, eye_color, hair_color, skin_color } =
     data;
 
-  const handleOnClick = (id) => {
+  const handleOnClick = (data) => {
+    const id = data.url.split('/').at(-2);
     navigate(`/people/${id}`);
   };
 
   return (
     <tr
       className="text-center hover:bg-gray-100 cursor-pointer"
-      onClick={() => handleOnClick(index)}
+      onClick={() => handleOnClick(data)}
     >
       <td className="border border-gray-400 p-2">{name}</td>
       <td className="border border-gray-400 p-2">{height} cm</td>
