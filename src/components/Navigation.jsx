29b38b375d0a220ff3328fdd1d59ae568927ce7e
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 22:27:38 +0100

    add filterInput to navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,9 @@
-// import { Link, NavLink } from 'react-router-dom';
-
-//owinąc w routeer
+import { FilterInput } from './FilterInput';
 
 export const Navigation = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-3 mb-3">
-      <div className="flex gap-2">
-        <p>Characters</p>
-        <p>Planets</p>
-      </div>
-      <div>
-        <input
-          type="text"
-          className="w-full px-4 py-2 text-gray-700 border rounded-xl shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-      </div>
+    <div className="w-fit mx-auto mb-3">
+      <FilterInput />
     </div>
   );
 };
