8364916af1d0937b72972354a4b79f6367b800ea
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 13:14:53 +0100

    delete if statement

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,6 +1,4 @@
-export const ErrorMessage = ({ error }) => {
-  if (!error) return null;
-
+export const ErrorMessage = () => {
   return (
     <div className="bg-red-100 border w-fit border-red-400 text-red-700 px-4 py-3 rounded mx-auto">
       <strong className="font-bold">Błąd podczas pobierania danych!</strong>
