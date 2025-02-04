d295958b9fbdfee7e2179252184332144ca41fb3
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 16:50:01 +0100

    create reusable header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,6 @@
-export const Header = () => (
+export const Header = ({ title, description }) => (
   <header className="bg-blue-600 text-white py-6 px-4 rounded-md shadow-md mb-4 text-center">
-    <h1 className="text-3xl font-bold">Star Wars Data Explorer</h1>
-    <p className="mt-2 text-lg">
-      Poznaj wszechświat Gwiezdnych Wojen! Przeglądaj dane dotyczące postaci i
-      planet. Skorzystaj z zakładek, aby przełączać się między kategoriami i
-      odkrywać fascynujące informacje.
-    </p>
+    <h1 className="text-3xl font-bold">{title}</h1>
+    <p className="mt-2 text-lg">{description}</p>
   </header>
 );
