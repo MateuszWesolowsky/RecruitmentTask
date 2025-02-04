f0a35c1581baf2004fc9835952122f223d02fb28
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 13:16:58 +0100

    fixed problem with onClicked row navigate

--- src/components/PeopleTableRow.jsx
+++ src/components/PeopleTableRow.jsx
@@ -1,15 +1,26 @@
-export const PeopleTableRow = ({ data }) => {
+import { useNavigate } from 'react-router-dom';
+
+export const PeopleTableRow = ({ data, index }) => {
+  const navigate = useNavigate();
+  const { name, height, mass, birth_year, eye_color, hair_color, skin_color } =
+    data;
+
+  const handleOnClick = (id) => {
+    navigate(`/people/${id}`);
+  };
+
   return (
-    <>
-      <tr className="text-center hover:bg-gray-100">
-        <td className="border border-gray-400 p-2">{data.name}</td>
-        <td className="border border-gray-400 p-2">{data.height} cm</td>
-        <td className="border border-gray-400 p-2">{data.mass} kg</td>
-        <td className="border border-gray-400 p-2">{data.birth_year}</td>
-        <td className="border border-gray-400 p-2">{data.eye_color}</td>
-        <td className="border border-gray-400 p-2">{data.hair_color}</td>
-        <td className="border border-gray-400 p-2">{data.skin_color}</td>
-      </tr>
-    </>
+    <tr
+      className="text-center hover:bg-gray-100 cursor-pointer"
+      onClick={() => handleOnClick(index)}
+    >
+      <td className="border border-gray-400 p-2">{name}</td>
+      <td className="border border-gray-400 p-2">{height} cm</td>
+      <td className="border border-gray-400 p-2">{mass} kg</td>
+      <td className="border border-gray-400 p-2">{birth_year}</td>
+      <td className="border border-gray-400 p-2">{eye_color}</td>
+      <td className="border border-gray-400 p-2">{hair_color}</td>
+      <td className="border border-gray-400 p-2">{skin_color}</td>
+    </tr>
   );
 };
