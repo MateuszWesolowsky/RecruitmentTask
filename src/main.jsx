7d139220df4ed93aae0246102a713cca2c37876c
Mateusz <m.wesolowski.92@gmail.com>
2025-02-04 13:14:09 +0100

    fixed routes & add contextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,35 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { SinglePage } from './pages/SinglePage';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+import { SingleViewPage } from './pages/SingleViewPage';
+import { DataProvider } from './context/DataProvider.jsx';
 import App from './App.jsx';
+import { NotFoundPage } from './pages/NotFoundPage.jsx';
 
 const router = createBrowserRouter([
   {
-    element: <App />,
+    element: <Navigate to="/people" />,
     path: '/',
+    errorElement: <NotFoundPage />,
   },
   {
-    element: <SinglePage />,
-    path: '/:id',
+    element: <App />,
+    path: '/people',
+  },
+  {
+    element: <SingleViewPage />,
+    path: '/people/:id',
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <DataProvider>
+      <RouterProvider router={router} />
+    </DataProvider>
   </StrictMode>,
 );
