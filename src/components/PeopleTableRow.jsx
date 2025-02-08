401d9263e2702fb687d1259ed688f7e714229d74
Mateusz <m.wesolowski.92@gmail.com>
2025-02-08 12:42:06 +0100

    Change destructuring to props

--- src/components/PeopleTableRow.jsx
+++ src/components/PeopleTableRow.jsx
@@ -1,27 +1,48 @@
 import { useNavigate } from 'react-router-dom';
 
-export const PeopleTableRow = ({ data }) => {
+export const PeopleTableRow = ({
+  data: {
+    name,
+    height,
+    mass,
+    birth_year,
+    eye_color,
+    hair_color,
+    skin_color,
+    url,
+  },
+}) => {
   const navigate = useNavigate();
-  const { name, height, mass, birth_year, eye_color, hair_color, skin_color } =
-    data;
 
-  const handleOnClick = (data) => {
-    const id = data.url.split('/').at(-2);
+  const handleOnClick = (url) => {
+    const id = url.split('/').at(-2);
     navigate(`/people/${id}`);
   };
 
   return (
     <tr
       className="text-center hover:bg-gray-100 cursor-pointer"
-      onClick={() => handleOnClick(data)}
+      onClick={() => handleOnClick(url)}
     >
-      <td className="border border-gray-400 p-2">{name}</td>
-      <td className="border border-gray-400 p-2">{height} cm</td>
-      <td className="border border-gray-400 p-2">{mass} kg</td>
-      <td className="border border-gray-400 p-2">{birth_year}</td>
-      <td className="border border-gray-400 p-2">{eye_color}</td>
-      <td className="border border-gray-400 p-2">{hair_color}</td>
-      <td className="border border-gray-400 p-2">{skin_color}</td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">{name}</td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {height} cm
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {mass} kg
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {birth_year}
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {eye_color}
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {hair_color}
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {skin_color}
+      </td>
     </tr>
   );
 };
