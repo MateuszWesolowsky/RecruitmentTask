b925fdca14c430d71b3c23930dd09bce55c10d4d
Mateusz <m.wesolowski.92@gmail.com>
2025-02-08 12:39:42 +0100

    Change currPege to Page from reducer & shortened btn fn

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -6,23 +6,31 @@ export const Pagination = () => {
   const { state, dispatch } = useContext(DataContext);
 
   const handlePreviousePageClick = () => {
-    if (state.currentPage > 1) {
-      dispatch({ type: 'SET_PAGE', payload: state.currentPage - 1 });
+    if (state.page > 1) {
+      dispatch({
+        type: 'SET_PAGE',
+        payload: state.page - 1,
+      });
     }
   };
+
   const handleNextPageClick = () => {
-    if (state.currentPage < state.totalPages) {
-      dispatch({ type: 'SET_PAGE', payload: state.currentPage + 1 });
+    if (state.page < state.totalPages) {
+      dispatch({
+        type: 'SET_PAGE',
+        payload: state.page + 1,
+      });
+      dispatch({ type: 'SET_SEARCH_VALUE', payload: '' });
     }
   };
 
   return (
     <div className="flex gap-4 justify-center items-center mt-4">
-      <Button onClick={() => handlePreviousePageClick()}>Previous</Button>
+      <Button onClick={handlePreviousePageClick}>Previous</Button>
       <p className="text-center">
-        Page {state.currentPage} of {state.totalPages}
+        Page {state.page} of {state.totalPages === 0 ? 1 : state.totalPages}
       </p>
-      <Button onClick={() => handleNextPageClick()}>Next</Button>
+      <Button onClick={handleNextPageClick}>Next</Button>
     </div>
   );
 };
