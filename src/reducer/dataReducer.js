d3903cf997710fd7ec77e51e441c3fdd1ce9ff0c
Mateusz <m.wesolowski.92@gmail.com>
2025-02-08 12:46:21 +0100

    Added page and inputSearch variables to initial state & logic to reducer

--- src/reducer/dataReducer.js
+++ src/reducer/dataReducer.js
@@ -3,7 +3,9 @@ export const initialState = {
   loading: false,
   error: null,
   currentPage: 1,
+  page: 1,
   totalPages: 1,
+  inputSearch: '',
 };
 
 export const dataReducer = (state, action) => {
@@ -19,10 +21,18 @@ export const dataReducer = (state, action) => {
       };
     case 'FETCH_ERROR':
       return { ...state, loading: false, error: action.payload };
-    case 'CHANGE_ENDPOINT':
-      return { ...state, endpoint: action.payload, currentPage: 1 };
     case 'SET_PAGE':
-      return { ...state, currentPage: action.payload };
+      return {
+        ...state,
+        page: action.payload,
+      };
+    case 'SET_PAGE_SEARCH':
+      return {
+        ...state,
+        currentPage: action.payload,
+      };
+    case 'SET_SEARCH_VALUE':
+      return { ...state, inputSearch: action.payload };
     default:
       return state;
   }
