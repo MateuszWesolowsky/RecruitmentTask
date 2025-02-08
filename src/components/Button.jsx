a689bae31721174eaf390c92ef0a2b8def84c854
Mateusz <m.wesolowski.92@gmail.com>
2025-02-08 12:34:48 +0100

    Delete return

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,8 @@
-export const Button = ({ onClick, children }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = ({ onClick, children }) => (
+  <button
+    onClick={onClick}
+    className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+  >
+    {children}
+  </button>
+);
